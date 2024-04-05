0b4700be9b7f6b54b7513f0588152a5f81874fc6
goran.codausi <goran.codausi@student.upt.ro>
2024-04-05 22:07:13 +0300

    added api users endpoint

--- backend/healthguard/api/models.py
+++ backend/healthguard/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class User(models.Model):
+    email = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
 
-# Create your models here.
+class MedicProfile(models.Model):
+    badge_number = models.CharField(max_length=50)
