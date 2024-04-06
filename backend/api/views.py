e122349e0013755a86455705b3d2512a640a3219
goran.codausi <goran.codausi@student.upt.ro>
2024-04-06 10:40:44 +0300

    restructured project

---
+++ backend/api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class ListUsersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class UpdateUserView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    lookup_field = 'pk'
--- backend/healthguard/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-class User(models.Model):
-    email = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=50)
-
-class MedicProfile(models.Model):
-    badge_number = models.CharField(max_length=50)
