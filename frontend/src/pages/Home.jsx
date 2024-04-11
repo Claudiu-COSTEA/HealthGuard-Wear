a708dd65637c7a3db65d6438bd17afe546f3ca5d
goran.codausi <goran.codausi@student.upt.ro>
2024-04-11 13:00:05 +0300

    moved html code to react components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,35 @@
 function Home() {
-    return <div>Home</div>
+  return (
+    <div>
+      <div class="bg-blue-500 text-white py-2 px-4 rounded-lg fixed top-0 left-0 w-full">
+        <div class="max-w-7xl mx-auto flex justify-between items-center">
+          <div class="text-lg font-semibold">HealthGuard Wear</div>
+          <div class="flex space-x-4">
+            <a href="#contact" class="hover:text-gray-300">
+              Contact
+            </a>
+            <a href="#help" class="hover:text-gray-300">
+              Help
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-cover h-screen flex items-center justify-center bg-white">
+        <div class="p-8 rounded-lg text-blue-900 text-center">
+          <h1 class="text-8xl font-bold mb-4">
+            Bun venit la HealthGuard Wear.
+          </h1>
+          <h2 class="text-6xl font-bold mb-4 text-blue-800">
+            Cum te putem ajuta?
+          </h2>
+          <button class="bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 rounded mt-20">
+            Intră în cont
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,48 @@
 function Login() {
-    return <div>Login</div>
+  return (
+    <div class="bg-cover h-screen flex items-center justify-center bg-white login-bg">
+      <div class="p-8 rounded-lg text-blue-800 text-center max-w-xl mx-auto">
+        <h1 class="text-3xl font-bold mb-4">Intrare în Cont</h1>
+        <form>
+          <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Nume Utilizator
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Nume Utilizator"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Parolă
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="**********"
+            />
+          </div>
+          <div class="mb-6">
+            <div class="flex justify-center">
+              <button class="bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 rounded">
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
