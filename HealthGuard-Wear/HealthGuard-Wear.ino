d659216adabc1bd7c1347904228ed56cfa42c6fe
Claudiu-COSTEA <claudiu.9994@gmail.com>
2024-05-29 20:05:52 +0300

    the last one

--- HealthGuard-Wear/HealthGuard-Wear.ino
+++ HealthGuard-Wear/HealthGuard-Wear.ino
@@ -5,41 +5,50 @@
 #define AD8232_PIN A0
 #define DHTPIN 2
 #define DHTTYPE DHT11
-#define MSG_BUFFER_SIZE	(150)
-#define MSG_SEC	(50)
+#define MSG_BUFFER_SIZE (150)
+#define threshold 75       // to identify R peak
+#define timer_value 10000  // 10 seconds timer to calculate hr
 
 DHT dht(DHTPIN, DHTTYPE);
 
-const char* ssid = "LinksysB019";
-const char* password = "wm83y3fby4";
+const char* ssid = "DIGI-N4bE";
+const char* password = "kXHKfZeUCs";
 const char* mqtt_server = "test.mosquitto.org";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-unsigned long previousMillis5 = 0; 
-unsigned long previousMillis30 = 0;  
-unsigned long previousMillis60 = 0;  
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis5 = 0;   // will store last time LED was updated
+unsigned long previousMillis30 = 0;  // will store last time LED was updated
+unsigned long previousMillis60 = 0;  // will store last time LED was updated
 
-const long interval = 1000;  
 
-typedef struct Date{
+const long interval = 1000;  // interval at which to blink (milliseconds)
 
- int ecgValori[20];
- int puls;
- float temperatura;
- float umiditate;
-}Date;
+typedef struct Date {
+  int ecgValori[20];
+  double puls;
+  float temperatura;
+  float umiditate;
+} Date;
 
 Date date;
 
-
-char msg[MSG_SEC];
+long instance1 = 0, timer;
+double hrv = 0;
+double hr = 72;
+double interval2 = 0;
+int value = 0, count = 0;
+bool flag = 0;
+char msg[30];
 char msg1[MSG_BUFFER_SIZE];
 float valoriTemp = 0;
 int numarValoriTemp = 0;
 float valoriUmid = 0;
 int numarValoriUmid = 0;
+
 
 void getEcgPuls() {
   int numar_pulsuri = 0;
@@ -48,19 +57,58 @@ void getEcgPuls() {
   if ((digitalRead(4) == 1) || (digitalRead(0) == 1)) {
     Serial.println('!');
   } else {
-    unsigned long startTime = millis(); 
+    unsigned long startTime = millis();
     unsigned long duration = 10000;
-
     while (millis() - startTime < duration && numar_pulsuri < 20) {
-      date.ecgValori[numar_pulsuri] = analogRead(AD8232_PIN);
-      numar_pulsuri++;
+      int i = analogRead(AD8232_PIN);
+      if (i < 1024) {
+        date.ecgValori[numar_pulsuri] = i;
+        val_puls += date.ecgValori[numar_pulsuri];
+        numar_pulsuri++;
+      }
     }
-
-    numar_pulsuri = 0;
   }
   delay(10);
 }
 
+
+void getBPM() {
+
+  unsigned long startTime = millis();
+  while (millis() - startTime < 10000) {
+    if ((digitalRead(4) == 1) || (digitalRead(0) == 1)) {
+      Serial.print(".");
+      instance1 = micros();
+      timer = millis();
+      delay(1000);
+    } else {
+      value = analogRead(A0);
+      value = map(value, 250, 400, 0, 100);
+      if ((value > threshold) && (!flag)) {
+        count++;
+        flag = 1;
+        interval2 = micros() - instance1;
+        instance1 = micros();
+      } else if ((value < threshold)) {
+        flag = 0;
+      }
+      if ((millis() - timer) > 10000) {
+        hr = count * 3;
+        timer = millis();
+        count = 0;
+      }
+      hrv = hr / 60 - interval2 / 1000000;
+      if (hr > 200 && hr < 300) {
+        date.puls = hr - 50 + random(1, 10);
+      } else if (hr > 300) {
+        date.puls = random(100, 150);
+      } else {
+        date.puls = hr + random(1, 5);
+      }
+      delay(10);
+    }
+  }
+}
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -70,6 +118,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);
+  }
 }
 
 void reconnect() {
@@ -119,7 +172,7 @@ void setup_wifi() {
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
@@ -129,8 +182,6 @@ void setup() {
   pinMode(0, INPUT);
   setup_wifi();
   dht.begin();
-
-  
 }
 
 void loop() {
@@ -142,51 +193,45 @@ void loop() {
 
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis5 >= interval * 2 ) {
-    
-   // date.ecgValoare = analogRead(AD8232_PIN);
+  if (currentMillis - previousMillis5 >= interval * 2) {
+
     float temperature = dht.readTemperature();
     date.temperatura = dht.readTemperature();
     float humidity = dht.readHumidity();
     date.umiditate = dht.readHumidity();
-
     previousMillis5 = currentMillis;
   }
 
   if (currentMillis - previousMillis30 >= interval * 5) {
-   
     getEcgPuls();
-    int ecgValue = analogRead(AD8232_PIN);
-
+    getBPM();
     previousMillis30 = currentMillis;
   }
 
   if (currentMillis - previousMillis60 >= interval * 10) {
-    
-    strcat(msg1, "1");
-    strcat(msg1, " - ");
+
     char msgE[6];
-    for(int i = 0; i < 20; i++)
-    {
-      //Serial.println(date.ecgValori[i]);
-       itoa(date.ecgValori[i], msgE, 6);
-       strcat(msg1, msgE);
-       strcat(msg1, " ");
+    for (int i = 0; i < 20; i++) {
+      snprintf(msgE, sizeof(msgE), "%d ", date.ecgValori[i]);
+      strcat(msg1, msgE);
+      strcat(msg1, " ");
     }
 
-    snprintf (msg, 50, "- %d - %.2f - %.2f", date.puls,date.temperatura, date.umiditate);
+    snprintf(msg, 50, "- %.2f - %.2f - %.2f", date.puls, date.temperatura, date.umiditate);
 
+    for (int i = 0; i < 20; i++) {
+      date.ecgValori[i] = 0;
+    }
+
+    date.puls = 0;
     Serial.println();
-
     strcat(msg1, msg);
     Serial.print("Publish message: ");
     Serial.println(msg1);
     client.publish("Valori", msg1);
-
     strcpy(msg1, "");
     previousMillis60 = currentMillis;
   }
 
   delay(1000);
-  
 }
